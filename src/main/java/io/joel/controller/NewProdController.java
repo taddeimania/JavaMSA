210c68783613b64410a0d9e7ca4c10aed4222642
taddeimania <jtaddei@gmail.com>
2017-09-10 20:51:15 -0400

    new endpoint

---
+++ src/main/java/io/joel/controller/NewProdController.java
@@ -0,0 +1,16 @@
+package io.joel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by taddeimania on 9/8/17.
+ */
+@RestController
+public class NewProdController {
+
+    @RequestMapping("/api/version")
+    public String getVersion() {
+        return "Production Branch!";
+    }
+}
